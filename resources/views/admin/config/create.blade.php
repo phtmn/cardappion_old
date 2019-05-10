3cce6fd02d935c58cc6a00fb3112f6d1304baab1
Pedro Henrique Tolentino de Melo Nogueira <pedro.nogueira@hotmail.com.br>
2019-05-10 00:08:21 -0300

    ajustes

--- resources/views/admin/config/create.blade.php
+++ resources/views/admin/config/create.blade.php
@@ -1,8 +1,24 @@
 @extends('layouts.admin.master')
 
 @section('cabecalho')
-    <div class="panel-header panel-header-sm">
+<div class="panel-header panel-header-sm">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        {{--<a href="{{ route('menus.create') }}" class="btn btn-outline-primary"> Novo Menu +</a> --}}
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            {{--<li class="breadcrumb-item"><a href="{{route('dashboard.index')}}">Cardappon</a></li>--}}
+                            <li class="breadcrumb-item active"><i class="text-success fa fa-cog nav-icon"></i> Perfil   </li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
     </div>
+
 @stop
 
 @section('conteudo')
@@ -10,20 +26,101 @@
  <form action="{{route('config.store')}}" method="POST">
     @csrf
     <div class="row">
-        <div class="col-md-6">
-            <div class="card">
+        <div class="col-md-12">
+            <div class="card card-success card-outline">
                 <div class="card-header">
-                    <h4 class="card-title">Informações Principais</h4>
+                    <h4 class="card-title"><b class=""> Dados do Estabelecimento </b></h4>
                 </div>
                 <div class="card-body">
-                    <div class="form-group">
-                        <label for="">Nome Da Loja</label>
-                        <input type="text" name="store_name" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="">Slogan</label>
-                        <input type="text" name="slogan" class="form-control">
-                    </div>
+
+
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Nome <b class="text-purple" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-6">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>               
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">CNPJ </label>
+                            <div class="col-sm-3">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">CEP <b class="text-success" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-2">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Rua/Logradouro <b class="text-primary" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-6">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Bairro <b class="text-primary" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-4">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Número <b class="text-primary" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-1">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Cidade <b class="text-primary" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-4">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Estado <b class="text-primary" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-1">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Telefone <b class="text-primary" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-2">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right"> WhatsApp <b class="text-primary" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-2">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Site <b class="text-primary" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-4">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Fan Page <b class="text-primary" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-4">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Instagram <b class="text-primary" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-4">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>                
+                
+                <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Segmento... ramo de atuação.. ou categoria <b class="text-primary" data-toggle="tooltip" data-placement="right" title="Dados obrigatórios"> * </b></label>
+                            <div class="col-sm-9">
+                                <input type="text" name="store_name" class="form-control">
+                            </div>
+                </div>
+
+                    
                     <div class="form-group">
                         <label for="category">Categoria</label>
                         <select name="category" id="" class="form-control">
@@ -34,67 +131,12 @@
                             <option value="Delivery">Delivery</option>
                         </select>
                     </div>
-                    <div class="form-group">
-                        <label for="description">Descreva sua Loja</label>
-                        <textarea name="description" id="" cols="30" rows="10" class="form-control"></textarea>
-                    </div>
+                                  
 
-                    <div class="form-check mt-3">
-                        <label class="form-check-label">
-                            <input class="form-check-input" type="checkbox" name="has_delivery" value="1">
-                            <span class="form-check-sign"></span>
-                                Faz Delivery?
-                        </label>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="delivery_phone">Número Delivery</label>
-                        <input type="text" name="delivery_phone" class="form-control">
-                    </div>
+                    <button type="submit" class="btn btn-block btn-success btn-lg btn-round"> Salvar Dados</button>
                 </div>
             </div>
-        </div>
-
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">Sociais e Comunicação</h4>
-                </div>
-                <div class="card-body">
-                    <div class="form-group">
-                        <input type="text" name="facebook" class="form-control" placeholder="facebook">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" name="instagram" class="form-control" placeholder="@intagram">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" name="maps" class="form-control" placeholder="maps.google">
-                    </div>
-
-                    <div class="form-group">
-                        <input type="text" name="whatsapp" class="form-control" placeholder="Whatsapp para pedidos">
-                    </div>
-
-                </div>
-            </div>
-
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">Endereço e Localização</h4>
-                </div>
-                <div class="card-body">
-                    <div class="form-group">
-                        <input type="text" name="address" class="form-control" placeholder="Endereço e numero">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" name="city" class="form-control" placeholder="Bairro e Cidade">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-12">
-            <button type="submit" class="btn btn-block btn-success btn-lg btn-round"> Salvar Dados</button>
-        </div>
+        </div>            
     </div>
 </form>
 @stop
--- resources/views/layouts/admin/partes/menu.blade.php
+++ resources/views/layouts/admin/partes/menu.blade.php
@@ -1,28 +1,35 @@
+
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <!-- Add icons to the links using the .nav-icon class
              with font-awesome or any other icon font library -->
         <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
-                <i class="nav-icon fa fa-plus"></i><p>Cadastros<i class="right fa fa-angle-left"></i></p>
-            </a>
+        <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="{{route('config.createEdit')}}" class="nav-link">
+                        <i class="text-success fa fa-cog nav-icon"></i>
+                        <p> Perfil</p>
+                    </a>
+                </li>
+            </ul>
+           
             <ul class="nav nav-treeview">
                 <li class="nav-item">
                     <a href="{{route('products.index')}}" class="nav-link">
-                        <i class="fa fa-group nav-icon"></i>
-                        <p>Produtos</p>
+                        <i class="text-warning fa fa-star nav-icon"></i>
+                        <p>Itens</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{route('menus.index')}}" class="nav-link">
-                        <i class="fa fa-group nav-icon"></i>
+                        <i class="text-primary fa fa-qrcode nav-icon"></i>
                         <p>Menus</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{route('promotions.index')}}" class="nav-link">
-                        <i class="fa fa-group nav-icon"></i>
-                        <p>Promoções</p>
+                        <i class="text-danger fa fa-bullhorn nav-icon"></i>
+                        <p>Ofertas</p>
                     </a>
                 </li>
             </ul>
@@ -30,22 +37,3 @@
     </ul>
 </nav>
 
-<nav class="mt-2">
-    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
-             with font-awesome or any other icon font library -->
-        <li class="nav-item has-treeview menu-open ">
-            <a href="#" class="nav-link active">
-                <i class="nav-icon fa fa-bar-chart "></i><p>Preferencias<i class="right fa fa-angle-left"></i></p>
-            </a>
-            <ul class="nav nav-treeview">
-                <li class="nav-item">
-                    <a href="{{route('config.createEdit')}}" class="nav-link">
-                        <i class="fa fa-gears nav-icon"></i>
-                        <p>Meus Dados</p>
-                    </a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-</nav>
